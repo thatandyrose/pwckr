81f16a28c5c5207971e929b3521fa67778da8a7c
andy rose <thatandyrose@gmail.com>
2013-12-07 10:38:55 +0800

    added ransperency to desktop photo info, started adding tests

---
+++ spec/features/searching_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'searching' do
+  feature 'As a user I can search from the home page' do
+    background do
+      visit root_path
+    end
+    scenario 'field' do
+    end
+  end
+end
